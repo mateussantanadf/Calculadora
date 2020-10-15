2430605392cf730d30a54e0b95cb71684c2248c7
mateussantanadf <santanamateus02@gmail.com>
2020-10-15 08:07:29 -0300

    Subindo exercicio para o repositorio

---
+++ test/AulaSwing/TesteThreadTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AulaSwing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mateus
+ */
+public class TesteThreadTest {
+    
+    public TesteThreadTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
